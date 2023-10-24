ac03a3c3ca0ba52e270747bcdfa49c085c4df5e1
satyasai69 <ch.satya.sai133@gmail.com>
2023-10-24 10:24:09 +0530

    foo

--- ifElse.sol
+++ ifElse.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.19;
 
 contract ifElse {
+  function foo(uint x) public pure returns(uint) {
+    if(x < 10) {
+       return 0;
+    }else if (x < 20) {
+        return 1;
+    }else{
+        return 2;
+    }
+  }
 
+  function ternaryz(uint _x) public pure returns(uint){
+    return _x < 10 ? 1 : 2;
+  }
 }
